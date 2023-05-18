9f8e7043798d1a7d6c018e04cd92a35f65da1bd5
Yaiza <yaiza.paez.garcia@gmail.com>
2023-05-18 09:43:51 +0200

    Repo clonat

--- llistaMat.js
+++ llistaMat.js
@@ -1,7 +1,10 @@
 function sum(list) {
+    if (list.length == 0) return 0
     let total = 0
-    list.array.forEach(element => {
+    list.forEach(element => {
         total += element
     });
     return total / list.length;
 }
+
+module.exports = { mean }
